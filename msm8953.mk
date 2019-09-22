d81112fc0f5400e98b1ede2b9e629791d8c79344
Bruno Martins <bgcngm@gmail.com>
2019-09-22 14:49:07 +0100

    msm8953-common: Switch to cryptfshw QSEECom HIDL
    Change-Id: Id1d580607a7d73ec6738ab3aa8dcdf9e27558a33

--- msm8953.mk
+++ msm8953.mk
@@ -201,7 +201,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
     android.hidl.manager@1.0
-    
+
+# HW crypto
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.cryptfshw@1.0-service-qti.qsee
+
 # IMS
 PRODUCT_PACKAGES += \
     ims-ext-common \
