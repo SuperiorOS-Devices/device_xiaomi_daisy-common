50a02f5c855f099c980a632d3ca495ad10a611a5
dianlujitao <dianlujitao@lineageos.org>
2020-03-26 17:56:38 +0100

    msm8953-common: Audio rework
    Downgraded soundtrigger, added a2dp and bluetooth audio stuff, and some other changes.

--- msm8953.mk
+++ msm8953.mk
@@ -89,20 +89,21 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@5.0-impl \
     android.hardware.soundtrigger@2.2-impl \
-    audio.a2dp.default \
-    audio.primary.msm8953 \
     audio.r_submix.default \
     audio.usb.default \
+    libaudioroute \
     libaacwrapper \
     libaudio-resampler \
     libqcomvisualizer \
     libqcomvoiceprocessing \
     libqcompostprocbundle \
-    tinymix
+    libvolumelistener
 
 # A2DP
 PRODUCT_PACKAGES += \
   audio.a2dp.default \
+  android.hardware.bluetooth.audio@2.0 \
+  android.hardware.bluetooth.audio@2.0-impl \
   android.hardware.bluetooth.a2dp@1.0-impl \
   android.hardware.bluetooth.a2dp@1.0-service
 
